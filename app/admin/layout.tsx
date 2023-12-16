4f44911a1c74ebc3671300714ff1be436d7eea8c
Omoshlawi <lawiomosh3@gmail.com>
2023-12-16 17:46:55 +0300

    Added admin dashboard bt not thee consistent

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,60 @@
-import React, { PropsWithChildren } from "react";
+"use client";
+import { useState, useEffect } from "react";
+import Loader from "@/app/admin/components/common/Loader";
 
-const AdminLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div>{children}</div>;
-};
+import Sidebar from "@/app/admin/components/Sidebar";
+import Header from "@/app/admin/components/Header";
 
-export default AdminLayout;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="flex h-screen overflow-hidden">
+              {/* <!-- ===== Sidebar Start ===== --> */}
+              <Sidebar
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+              />
+              {/* <!-- ===== Sidebar End ===== --> */}
+
+              {/* <!-- ===== Content Area Start ===== --> */}
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                {/* <!-- ===== Header Start ===== --> */}
+                <Header
+                  sidebarOpen={sidebarOpen}
+                  setSidebarOpen={setSidebarOpen}
+                />
+                {/* <!-- ===== Header End ===== --> */}
+
+                {/* <!-- ===== Main Content Start ===== --> */}
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                    {children}
+                  </div>
+                </main>
+                {/* <!-- ===== Main Content End ===== --> */}
+              </div>
+              {/* <!-- ===== Content Area End ===== --> */}
+            </div>
+          )}
+        </div>
+      </body>
+    </html>
+  );
+}
